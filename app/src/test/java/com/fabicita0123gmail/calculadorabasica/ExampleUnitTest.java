fa6b9f6c85ee91c1705a33c11d517894c3b66e72
RuthFaviola <fabicita0123@gmail.com>
2017-05-02 18:52:09 -0400

    Commit inicial

---
+++ app/src/test/java/com/fabicita0123gmail/calculadorabasica/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.fabicita0123gmail.calculadorabasica;
+
+    import android.app.Application;
+    import android.test.ApplicationTestCase;
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
